60a30f2ceaed5e912ed831bdaf1b85cd60e85b64
jmin111301 <jonathanheromin@gmail.com>
2024-10-02 09:21:27 -0400

    added agentic structure

--- proto.py
+++ proto.py
@@ -1,12 +1,21 @@
 import docx
 import re
-from src.contextparser import Context, Contexts, ContextParser
 from pypdf import PdfReader
+from urllib.request import urlopen
+from langchain_community.document_transformers import Html2TextTransformer
+from langchain_community.document_loaders import AsyncHtmlLoader
 
 def main():
-    reader = PdfReader("resume.pdf")
-    text = "\n".join([page.extract_text() for page in reader.pages])
-    print(text)
+    url = "https://jobs.fidelity.com/job-details/21056911/associate-data-scientist/"
+    urls = [url]
+    loader = AsyncHtmlLoader(urls)
+    docs = loader.load()
+    html2text = Html2TextTransformer()
+    docs_transformed = html2text.transform_documents(docs)
+
+    print(docs_transformed)
+    print(docs_transformed[0].page_content)
+
 
 if __name__ == '__main__':
     main()
