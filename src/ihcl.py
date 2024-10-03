64f19b3454f9d9480eea01901edfa5cb5a694b42
jmin111301 <jonathanheromin@gmail.com>
2024-10-02 21:56:25 -0400

    updated cli arguments + functionality

--- src/ihcl.py
+++ src/ihcl.py
@@ -11,7 +11,9 @@ project_root = os.path.dirname(os.path.abspath(__file__))
 # Add the 'src' directory to the Python path
 src_dir = os.path.join(project_root, 'src')
 sys.path.append(src_dir)
-from contexts import Contexts
+from contexts import Contexts, Context
+from cleaner import ContextAgentState, Contextifier
+from langchain_openai import ChatOpenAI
 
 # TODO: eventually switch to an ollama model 
 model = ChatOpenAI(model="gpt-4")
@@ -21,9 +23,11 @@ app = typer.Typer()
 @app.command()
 def contextify(
         contextf: Annotated[str, typer.Argument(help="The file that contains a (description, path) pair. The structure should follow DESCRIPTION DELIM PATH")], 
-        delim: Annotated[str, typer.Option("--delim", "-d", help= "The delimiter for contextf")] = ",", 
         templatef: Annotated[str, typer.Argument(help="The template file which contains fields surrounded by a bracket that will be filled based on context and the template")],
         bracket: Annotated[Tuple[str, str], typer.Argument(help="The pair of brackets which identify fields in the template that will be filled with context")],
+        delim: Annotated[str, typer.Option("--delim", "-d", help= "The delimiter for contextf")] = ",",
+        hitl: Annotated[bool, typer.Option("--hitl", "-h", help= "Option for human in the loop workflow")] = False,
+        log: Annotated[str, typer.Option("--log", "-l", help= "Filename for log")] = None
         # human-in-the-loop option
         # tools
     ):
@@ -31,7 +35,8 @@ def contextify(
     parsed_contexts = Contexts(contextf, delim)
     for context in parsed_contexts.contexts:
         print(context)
-    # parsed_template = Context("template", template_f)
+    parsed_template = Context("template", templatef)
+    print(parsed_template)
 
     
 if __name__ == "__main__":
