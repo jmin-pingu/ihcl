d23881a22c3f6ccbe7b7d2fedf93f8b89d6fee9c
jmin111301 <jonathanheromin@gmail.com>
2024-10-05 22:46:35 -0400

    updated config

--- proto.py
+++ proto.py
@@ -4,18 +4,27 @@ from pypdf import PdfReader
 from urllib.request import urlopen
 from langchain_community.document_transformers import Html2TextTransformer
 from langchain_community.document_loaders import AsyncHtmlLoader
+from typing import TypedDict, List
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+class Context(BaseModel):
+    description: str = Field(description="The description about what is contained in this Context object")
+    content: str = Field(description="The content of the context object")
+
+
+class CleanedContext(BaseModel):
+    cleaned: bool = Field(description="Whether the Context was cleaned or not")
+    context: Context = Field(description="A Context object")
+
+class ContextAgentState(BaseModel):
+    cleaned_contexts: List[CleanedContext] = Field(description= "A list of CleanedContext objects")# descriptions: guidelines for acting on content
 
 def main():
-    url = "https://jobs.fidelity.com/job-details/21056911/associate-data-scientist/"
-    urls = [url]
-    loader = AsyncHtmlLoader(urls)
-    docs = loader.load()
-    html2text = Html2TextTransformer()
-    docs_transformed = html2text.transform_documents(docs)
-
-    print(docs_transformed)
-    print(docs_transformed[0].page_content)
-
+    data: ContextAgentState = {
+            "cleaned_contexts": {"cleaned": False, 
+                                 "context": {"description":"test_desc", "content": "test_content"}}
+    }
+    print(data)
 
 if __name__ == '__main__':
     main()
